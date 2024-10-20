9dca0a10a55322b989c4f6a8a17b2a7f271e313a
lost <lost>
2024-10-20 21:50:44 +0800

    [nim] buffer

--- hello_nim/buffer.nim
+++ hello_nim/buffer.nim
@@ -4,8 +4,9 @@ type BufferPayload[T] = object
   cap: int
   data: UncheckedArray[T]
 
+# Note:
+# current implementaion is based on the assumpation: align = 8B
 proc sizeOfBufferPayload[T](cap: int): int =
-  # align(sizeof(int), alignof(T)) + cap * sizeof(T)
   result = sizeof(int) + cap * sizeof(T)
   let align = alignof(int)
   let `mod` = result mod align
@@ -14,62 +15,89 @@ proc sizeOfBufferPayload[T](cap: int): int =
 
 type Buffer*[T] = object
   p: ptr BufferPayload[T]
-  rc, wc: int
+  head, tail: int
+  len: int
 
 proc `=destroy`[T](buf: Buffer[T]) =
   if buf.p != nil:
-    for i in (buf.rc ..< buf.wc):
-      let rrc = i mod buf.cap
-      `=destroy`(buf.p.data[rrc])
+    for i in (buf.head ..< buf.tail):
+      `=destroy`(buf.p.data[i])
     dealloc(buf.p)
 
 proc `=copy`[T](
   dst: var Buffer[T], src: Buffer[T]
 ) {.error: "Buffer[T] cannot be copied".}
 
-proc createBufferPayload[T](cap: int): ptr BufferPayload[T] =
+proc createBufferPayload[T](cap: int): ptr BufferPayload[T] {.inline.} =
   result = cast[ptr BufferPayload[T]](alloc0(sizeOfBufferPayload[T](cap)))
   result.cap = cap
 
-proc initBuffer*[T](cap: int): Buffer[T] =
+proc initBuffer*[T](cap: int): Buffer[T] {.inline.} =
   result.p = createBufferPayload[T](cap)
 
-proc cap*[T](buf: Buffer[T]): int =
+proc cap*[T](buf: Buffer[T]): int {.inline.} =
   buf.p.cap
 
-proc len*[T](buf: Buffer[T]): int =
-  buf.wc - buf.rc
-
-proc rrc[T](buf: Buffer[T]): int =
-  buf.rc mod buf.cap
-
-proc rwc[T](buf: Buffer[T]): int =
-  buf.wc mod buf.cap
-
-proc isEmpty*[T](buf: Buffer[T]): bool =
+proc isEmpty*[T](buf: Buffer[T]): bool {.inline.} =
   buf.len == 0
 
-proc isFull*[T](buf: Buffer[T]): bool =
+proc isFull*[T](buf: Buffer[T]): bool {.inline.} =
   buf.len == buf.cap
 
-proc read*[T](buf: var Buffer[T]): Option[T] =
+proc delHead*[T](buf: var Buffer[T]): Option[T] =
   if buf.isEmpty:
     return none(T)
-  result = some(buf.p.data[buf.rrc])
-  inc buf.rc
+  let head = buf.head
+  result = some(buf.p.data[head])
+  buf.head = (head + 1) mod buf.cap
+  dec buf.len
 
-proc write*[T](buf: var Buffer[T], value: sink T) =
+proc addHead*[T](buf: var Buffer[T], value: sink T) =
   if buf.isFull:
-    raise newException(ValueError, "WriteErr: buffer is full")
-  buf.p.data[buf.rwc] = value
-  inc buf.wc
+    raise newException(ValueError, "addHead: buffer is full")
+  let head = (buf.head + buf.cap - 1) mod buf.cap
+  buf.p.data[head] = value
+  buf.head = head
+  inc buf.len
+
+proc delTail*[T](buf: var Buffer[T]): Option[T] =
+  if buf.isEmpty:
+    return none(T)
+  let tail = (buf.tail + buf.cap - 1) mod buf.cap
+  result = some(buf.p.data[tail])
+  buf.tail = tail
+  dec buf.len
+
+proc addTail*[T](buf: var Buffer[T], value: sink T) =
+  if buf.isFull:
+    raise newException(ValueError, "addTail: buffer is full")
+  let tail = buf.tail
+  buf.p.data[tail] = value
+  buf.tail = (tail + 1) mod buf.cap
+  inc buf.len
+
+################
+## read/write ##
+################
+
+proc read*[T](buf: var Buffer[T]): Option[T] {.inline.} =
+  buf.delHead()
+
+proc write*[T](buf: var BUffer[T], value: sink T) {.inline.} =
+  buf.addTail()
 
 ##############
 ## operator ##
 ##############
 
-proc `<<`*[T](buf: var Buffer[T], value: sink T) =
-  buf.write(value)
+proc `<<`*[T](buf: var Buffer[T], value: sink T) {.inline.} =
+  buf.addTail(value)
+
+proc `<<|`*[T](list: var Buffer[T], value: sink T) {.inline.} =
+  list.addHead(value)
+
+proc `>>`*[T](value: sink T, list: var Buffer[T]) {.inline.} =
+  list.addHead(value)
 
 #############
 ## testing ##
@@ -93,3 +121,16 @@ doAssert buf.read == "foo".some
 doAssert buf.read == "bar".some
 doAssert buf.read == none(string)
 doAssert buf.len == 0
+
+"halo" >> buf
+"foo" >> buf
+buf << "bar"
+
+doAssertRaises(ValueError):
+  "zzz" >> buf
+
+doAssert buf.len == 3
+doAssert buf.delTail == "bar".some
+doAssert buf.delHead == "foo".some
+doAssert buf.delTail == "halo".some
+doAssert buf.delTail == none(string)
