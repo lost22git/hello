09efc9375c9fdf4e797b59251533f81ff1dde55c
lost <lost>
2024-10-23 00:30:15 +0800

    [crystal] buffer

---
+++ hello_crystal/buffer.cr
@@ -0,0 +1,132 @@
+struct Buffer(T)
+  include Indexable::Mutable(T)
+  getter cap : Int32
+
+  def initialize(@cap : Int32)
+    @data = Pointer(T).malloc(cap)
+    @head = @tail = 0
+  end
+
+  @[AlwaysInline]
+  def unsafe_fetch(index : Int)
+    @data[mask(@head + index)]
+  end
+
+  @[AlwaysInline]
+  def unsafe_put(index : Int, value : T)
+    @data[mask(@head + index)] = value
+  end
+
+  def size : Int32
+    result = @tail - @head
+    return result if result >= 0
+    result + @cap * 2
+  end
+
+  @[AlwaysInline]
+  def empty? : Bool
+    size() == 0
+  end
+
+  @[AlwaysInline]
+  def full? : Bool
+    size() == @cap
+  end
+
+  @[AlwaysInline]
+  private def mask(index : Int32) : Int32
+    index % @cap
+  end
+
+  @[AlwaysInline]
+  private def mask2(index : Int32) : Int32
+    index % (2 * @cap)
+  end
+
+  @[AlwaysInline]
+  private def dec(index : Int32, count : Int32 = 1) : Int32
+    index + (2 * @cap) - count
+  end
+
+  @[AlwaysInline]
+  private def inc(index : Int32, count : Int32 = 1) : Int32
+    index + count
+  end
+
+  def add_head(value : T)
+    raise "addHead: Buffer is full" if full?
+    @head = head = mask2(dec(@head))
+    # puts "[add_head] #{self}"
+    @data[mask(head)] = value
+  end
+
+  def del_head : T?
+    return nil if empty?
+    head = @head
+    @head = mask2(inc(head))
+    # puts "[del_head] #{self}"
+    @data[mask(head)]
+  end
+
+  def add_tail(value : T)
+    raise "addTail: Buffer is full" if full?
+    tail = @tail
+    @tail = mask2(inc(tail))
+    # puts "[add_tail] #{self}"
+    @data[mask(tail)] = value
+  end
+
+  def del_tail : T?
+    return nil if empty?
+    @tail = tail = mask2(dec(@tail))
+    # puts "[del_tail] #{self}"
+    @data[mask(tail)]
+  end
+end
+
+require "spec"
+
+describe Buffer do
+  it "add/del" do
+    buf = Buffer(String).new(3)
+    buf.size.should eq 0
+    buf.empty?.should be_true
+
+    buf.del_head.should be_nil
+    buf.del_tail.should be_nil
+
+    buf.add_head "halo"
+    buf.size.should eq 1
+    buf.add_tail "foo"
+    buf.size.should eq 2
+    buf.add_tail "bar"
+    buf.size.should eq 3
+    buf.full?.should be_true
+    expect_raises Exception do
+      buf.add_head "zzz"
+    end
+
+    buf.del_head.should eq "halo"
+    buf.size.should eq 2
+    buf.del_tail.should eq "bar"
+    buf.size.should eq 1
+    buf.del_head.should eq "foo"
+    buf.size.should eq 0
+    buf.empty?.should be_true
+    buf.del_tail.should be_nil
+  end
+
+  it "indexable" do
+    buf = Buffer(String).new(3)
+    buf.add_tail "foo"
+    buf.add_tail "bar"
+
+    buf[0] = "zzz"
+    buf[0].should eq "zzz"
+    buf[2]?.should be_nil
+
+    buf.each_with_index do |i, v|
+      v.should eq "bar" if i == 1
+    end
+  end
+end
