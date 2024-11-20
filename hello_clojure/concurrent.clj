588e801bdd495631d06890d2940cf8402bbf1f76
lost <lost>
2024-11-20 22:22:35 +0800

    [clojure] update

---
+++ hello_clojure/concurrent.clj
@@ -0,0 +1,40 @@
+#!/usr/bin/env -S clj -M
+
+; atom
+(def cnt (atom 0 :validator #(>= % 0)))
+(dotimes [_ 1000]
+  (future (swap! cnt inc)))
+(. java.lang.Thread sleep 500)
+(assert (= @cnt 1000))
+
+; ref
+(defn transfer [from to cnt]
+  (dosync
+   (alter from - cnt)
+   (alter to + cnt)))
+
+(def from (ref 1000 :validator #(>= % 0)))
+(def to (ref 1000 :validator #(>= % 0)))
+
+(dotimes [_ 1000]
+  (future (transfer from to 1)))
+(. java.lang.Thread sleep 500)
+(assert (= @from 0))
+(assert (= @to 2000))
+
+; agent
+(def cnt (agent 0 :validator #(>= % 0)))
+(send cnt inc)
+(send cnt inc)
+(send cnt inc)
+(. java.lang.Thread sleep 500)
+(assert (= @cnt 3))
+
+; java object locking
+(def list (java.util.ArrayList.))
+(assert (= (.size list) 0))
+(dotimes [i 1000]
+  (future (locking list
+            (.add list i))))
+(. java.lang.Thread sleep 500)
+(assert (= (.size list) 1000))
