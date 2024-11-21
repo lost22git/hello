94350b017fef61c62ffbfbb615388b0a1106e547
lost <lost>
2024-11-21 17:36:20 +0800

    [clojure] update

--- hello_clojure/log.clj
+++ hello_clojure/log.clj
@@ -1,7 +1,6 @@
 #!/usr/bin/env bb
 
-(ns test
-  (:require [clojure.tools.logging :as log]))
+(require '[clojure.tools.logging :as log])
 
 (log/info "halo" "clojure")
 (log/infof "%s %s" "halo" "clojure")
