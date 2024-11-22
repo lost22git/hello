58edbbf65cdd4c10f7e931a0a05763a513e40623
lost <lost>
2024-11-22 22:31:47 +0800

    [clojure] update

---
+++ hello_clojure/bb-http-client.clj
@@ -0,0 +1,23 @@
+#!/usr/bin/env bb
+(require '[clojure.pprint :as pp :refer [pprint]])
+(require '[babashka.http-client :as http])
+(require '[cheshire.core :as json])
+
+; get
+(assert
+ (= 888
+    (:status (http/get "https://httpbin.org/status/888" {:throw false}))))
+
+; post json and decode json resp body
+(-> (http/post "https://httpbin.org/post"
+                          {:header {:content-type "application/json"}
+                           :body (json/encode {:foo "bar"})})
+    :body
+    (json/parse-string true)
+    :data
+    (json/parse-string true)
+    :foo
+    (= "bar")
+    assert
+ )
+
