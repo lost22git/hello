ae0595d254bcd3d91c119eb278f044238172d5b7
lost <lost>
2024-12-01 02:16:43 +0800

    [clojure] update

--- hello_clojure/macros.clj
+++ hello_clojure/macros.clj
@@ -1,13 +1,22 @@
 #!/usr/bin/env -S clj -M
 
+; define macro
 (defmacro pp [expr]
   `(println ~(str expr) "=>" ~expr))
 
+; expand macro
+(macroexpand '(pp (-> (Thread/currentThread)
+                      (.getName))))
+
+; call macro
 (pp (-> (Thread/currentThread)
         (.getName)))
 
 (defmacro pp2 [expr]
   `(println (str '~expr) "=>" ~expr))
+
+(macroexpand '(pp2 (-> (Thread/currentThread)
+                       (.getName))))
 
 (pp2 (-> (Thread/currentThread)
          (.getName)))
