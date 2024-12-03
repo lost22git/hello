18a44712c10bd2ac9cc158371915d890eee4bd25
lost <lost>
2024-12-03 21:09:08 +0800

    [clojure] update

--- hello_clojure/macros.clj
+++ hello_clojure/macros.clj
@@ -15,9 +15,19 @@
 (defmacro pp2 [expr]
   `(println (str '~expr) "=>" ~expr))
 
+; pp2
 (macroexpand '(pp2 (-> (Thread/currentThread)
                        (.getName))))
 
 (pp2 (-> (Thread/currentThread)
          (.getName)))
 
+; pp3
+(defmacro pp3 [expr]
+  (list 'println (str expr) "=>" expr))
+
+(macroexpand '(pp3 (-> (Thread/currentThread)
+                       (.getName))))
+
+(pp3 (-> (Thread/currentThread)
+         (.getName)))
