d0cb5d2212faa20f2a9170e1da5fe68b0668e4db
lost <lost>
2024-12-06 06:08:54 +0800

    [dart] init

---
+++ hello_dart/README.md
@@ -0,0 +1,14 @@
+# Dart Lang Learning
+
+## Compilation
+
+[`dart compile`](https://dart.dev/tools/dart-compile)
+
+- `exe`: native, self-contained, with small runtime, run with `./demo.exe`
+- `aot-snapshot`: native, without runtime, run with `dartaotruntime demo.aot`
+- `jit-snapshot`: native and run with `dart demo.jit` 
+- `kernel`: cross-platform, IR and run with `dart demo.dill`
+- `js`: js and run with `node demo.js`
+- `wasm` wasm and run with `node demo.wasm`
+
+
---
+++ hello_dart/type.dart
@@ -0,0 +1,59 @@
+#!/usr/bin/env -S dart --enable-asserts
+
+void main() {
+  // Null
+  assert(null.runtimeType == Null);
+  assert(null is Null);
+
+  // bool
+  assert(true.runtimeType == bool);
+  assert(true is bool);
+
+  // int
+  assert((-1).runtimeType == int);
+  assert((-1) is int);
+
+  // double
+  assert((-1.1).runtimeType == double);
+  assert((-1.1) is double);
+
+  // record (.aka tuple)
+  assert((1, name: "dart", true).runtimeType.toString() ==
+      "(int, bool, {String name})");
+  assert((1, name: "dart", true) is (int, bool, {String name}));
+
+  // list
+  assert([].runtimeType == List<dynamic>);
+  assert(<int>[].runtimeType == List<int>);
+  assert([] is List<dynamic>);
+  assert(<int>[] is List<int>);
+
+  // map
+  assert({"a": 1, "b": 2}.runtimeType.toString() == "_Map<String, int>");
+  assert({"a": 1, "b": 2} is Map<String, int>);
+
+  // set
+  assert({1, 2, 3}.runtimeType.toString() == "_Set<int>");
+  assert({1, 2, 3} is Set<int>);
+
+  // class
+  assert(Book(1, "hello dart").runtimeType == Book);
+  assert(Book(1, "hello dart") is Book);
+
+  // nullable
+  Book? book = null;
+  book = Book(1, "hello dart");
+  assert(book.runtimeType == Book);
+  assert(book is Book);
+  book = null;
+  assert(book.runtimeType == Null);
+  assert(book is Null);
+}
+
+class Book {
+  final int id;
+  final String name;
+
+  Book(this.id, this.name);
+  String toString() => 'Book{:id ${id} :name "${name}"}';
+}
