d0d9a427caa50a06198424edcd9f81c26079b8e6
lost <lost>
2024-12-11 23:20:43 +0800

    [clojure] neil-demo

---
+++ hello_clojure/neil-demo/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to io.github.lost/neil-demo
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ hello_clojure/neil-demo/test/lost/neil_demo_test.clj
@@ -0,0 +1,7 @@
+(ns lost.neil-demo-test
+  (:require [clojure.test :refer :all]
+            [lost.neil-demo :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
