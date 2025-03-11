2717e031bb2cdba7242331ac861caae504052454
lost <lost>
2025-03-11 19:18:15 +0800

    [clojure] init clojurescript-demo

---
+++ clojure/clojurescript-demo/README.md
@@ -0,0 +1,6 @@
+# ClojuerScript Demo
+
+## Resources
+
+- [quick-start](https://clojurescript.org/guides/quick-start)
+- [reference](https://clojurescript.org/reference/compiler-options)
---
+++ clojure/clojurescript-demo/src/clojurescript_demo/core.cljs
@@ -0,0 +1,8 @@
+(ns clojurescript-demo.core)
+
+(js/alert "; Hello, Clojurescript on WebBrowser")
+(js/console.log "; Hello, Clojurescript on WebBrowser")
+
+; (println "; Hello, Clojurescript on Nodejs")
+
+
