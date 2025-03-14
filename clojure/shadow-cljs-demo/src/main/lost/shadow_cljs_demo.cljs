e6cf569fc664557f06f070430fcb6ddf2238f99b
lost <lost>
2025-03-15 01:43:37 +0800

    [clojure] shadow-cljs-demo: add re-frame-http-fx

--- clojure/shadow-cljs-demo/src/main/lost/shadow_cljs_demo.cljs
+++ clojure/shadow-cljs-demo/src/main/lost/shadow_cljs_demo.cljs
@@ -1,28 +1,46 @@
 (ns lost.shadow-cljs-demo
   (:require [reagent.dom.client :as rdom]
             [re-frame.core :as rf]
+            [day8.re-frame.http-fx]
+            [ajax.core :as ajax]
             [cljs.core :as c]))
 
 ; (defn -main [& _]
 ;   (println "Hello NodeJS, Love from Shadow-cljs")
 ;   (js/console.log "Console logging..."))
 
-;; === Event Handlers ===
+;; === Event Handler ===
 
 (rf/reg-event-db ::init (fn [_ _] {:cats []}))
 
-(rf/reg-event-db ::inc-count
-                 (fn [db _]
-                   (c/update db :cats c/conj {:url "https://cdn2.thecatapi.com/images/W3fUcHI8z.jpg"})))
+(rf/reg-event-fx ::inc-count
+                 (fn [_ _]
+                   {:http-xhrio {:method :get
+                                 :uri "https://api.thecatapi.com/v1/images/search"
+                                 :timeout 3000
+                                 :response-format (ajax/json-response-format {:keywords? true})
+                                 :on-success [::fetch-cat-ok]
+                                 :on-failure [:bad-http-result]}}))
+
+(rf/reg-event-db ::fetch-cat-ok
+                 (fn [db [_ data]]
+                   (c/update db :cats c/into data)))
 
 (rf/reg-event-db ::dec-count
                  (fn [db _]
                    (c/update db :cats c/pop)))
 
-;; === View ===
+;; === Subscrition ===
+
+(rf/reg-sub ::cat-list
+            (fn [db _]
+              (:cats db)))
 
 (rf/reg-sub ::cats-count
-            (fn [db _] (count (:cats db))))
+            :<- [::cat-list] ;; sugar of (fn [_ _] (rf/subscribe [::cat-list]))
+            (fn [cat-list _] (count cat-list)))
+
+;; === View ===
 
 (defn cat-count-view []
   (let [count @(rf/subscribe [::cats-count])]
@@ -33,10 +51,6 @@
      [:button {:disabled (zero? count)
                :on-click #(rf/dispatch [::dec-count])}
       "-"]]))
-
-(rf/reg-sub ::cat-list
-            (fn [db _]
-              (:cats db)))
 
 (defn cat-list-view []
   (let [cats @(rf/subscribe [::cat-list])]
