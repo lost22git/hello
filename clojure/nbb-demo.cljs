68503cea786f1103f462797096c2d160ac7979af
lost <lost>
2025-03-16 21:59:53 +0800

    [clojure] init nbb-demo.cljs

---
+++ clojure/nbb-demo.cljs
@@ -0,0 +1,19 @@
+#!/usr/bin/env nbb
+
+(ns nbb-demo
+  (:require [promesa.core :as p]))
+
+(defn json-stringify-data []
+  (->
+   #js {:name "nbb" :time (js/Date.)}
+   (js/JSON.stringify)))
+
+(defn fetch-data []
+  (p/->  (js/fetch "https://httpbin.org/post"
+                   #js {:method "POST"
+                        :headers #js {:content-type "application/json"}
+                        :body (json-stringify-data)})
+         (.json)))
+
+(p/-> (fetch-data)
+      js/console.log)
