71c8d508e1f53f1fb28f1777ca379ab706aa2b68
lost <lost>
2025-03-19 20:56:11 +0800

    [clojure] update squint-demo

--- clojure/squint-demo/vite.config.js
+++ clojure/squint-demo/vite.config.js
@@ -4,4 +4,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
