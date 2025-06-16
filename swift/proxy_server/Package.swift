f6a29a6a215e46d17d0d171cdf76cde1cc9d38dc
lost <lost>
2025-06-17 00:57:48 +0800

    [swift] vapor

---
+++ swift/proxy_server/Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version:6.0
+import PackageDescription
+
+let package = Package(
+    name: "proxy_server",
+    platforms: [
+       .macOS(.v13)
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.115.0"),
+        // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "proxy_server",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .testTarget(
+            name: "proxy_serverTests",
+            dependencies: [
+                .target(name: "proxy_server"),
+                .product(name: "VaporTesting", package: "vapor"),
+            ],
+            swiftSettings: swiftSettings
+        )
+    ]
+)
+
+var swiftSettings: [SwiftSetting] { [
+    .enableUpcomingFeature("ExistentialAny"),
+] }
---
+++ swift/proxy_server/Sources/proxy_server/routes.swift
@@ -0,0 +1,30 @@
+import Vapor
+
+func routes(_ app: Application) throws {
+    app.get { req async in
+        "It works!"
+    }
+
+    app.get("hello") { req async -> String in
+        "Hello, world!"
+    }
+    
+    app.get("proxy") { req async throws -> ClientResponse in
+        guard let target: String = try req.query.get(at: "target") else {
+            throw Abort(.badRequest, reason: "Argument not found: `target`")
+        }
+        req.logger.info("Fetching \(target)")
+        return try await req.client.get(URI(string: target))
+    }
+
+    app.post("proxy") { req async throws -> ClientResponse in
+        guard let target: String = try req.query.get(at: "target") else {
+            throw Abort(.badRequest, reason: "Argument not found: `target`")
+        }
+        req.logger.info("Fetching \(target)")
+        return try await req.client.post(URI(string: target)) { creq in
+            creq.headers = req.headers
+            creq.body = req.body.data
+        }
+    }
+}
