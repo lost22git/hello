6ec8a06734bcbeceba51085a6fa0727ddf921942
lost <lost>
2025-06-25 05:21:45 +0800

    [swift] update proxy_server

--- swift/proxy_server/Sources/proxy_server/configure.swift
+++ swift/proxy_server/Sources/proxy_server/configure.swift
@@ -5,6 +5,9 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    app.http.client.configuration.timeout.connect = .seconds(30)
+    app.http.client.configuration.timeout.read = .seconds(30)
+    
     // register routes
     try routes(app)
 }
--- swift/proxy_server/Sources/proxy_server/routes.swift
+++ swift/proxy_server/Sources/proxy_server/routes.swift
@@ -17,13 +17,15 @@ func routes(_ app: Application) throws {
         return try await req.client.get(URI(string: target))
     }
 
-    app.post("proxy") { req async throws -> ClientResponse in
+    app.on(.POST,"proxy", body: .collect(maxSize: "1mb")) { req async throws -> ClientResponse in
         guard let target: String = try req.query.get(at: "target") else {
             throw Abort(.badRequest, reason: "Argument not found: `target`")
         }
         req.logger.info("Fetching \(target)")
-        return try await req.client.post(URI(string: target)) { creq in
+        let targetUrl = URI(string: target)
+        return try await req.client.post(targetUrl) { creq in
             creq.headers = req.headers
+            creq.headers.replaceOrAdd(name: .host, value: targetUrl.host!)
             creq.body = req.body.data
         }
     }
