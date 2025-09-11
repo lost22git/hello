d561784bc28b832c7b00e2783f4f6e5c13525d5e
lost <lost>
2025-09-12 02:05:20 +0800

    [swift] update

--- swift/README.md
+++ swift/README.md
@@ -7,3 +7,4 @@
 - https://docs.swift.org/compiler/documentation/diagnostics/temporary-pointers/
 - https://www.vadimbulavin.com/swift-pointers-overview-unsafe-buffer-raw-and-managed-pointers/
 - https://www.kodeco.com/7181017-unsafe-swift-using-pointers-and-interacting-with-c
+- https://medium.com/@fred.j/swift-actors-in-depth-19c8b3dbd85a
--- swift/actor.swift
+++ swift/actor.swift
@@ -1,15 +1,61 @@
-actor Counter {
-    private(set) var value: Int = 0
+#!/usr/bin/env swift
 
-    func update(by: Int) {
-        value += by
+actor Bucket {
+   private var map: [String:String] = [:]
+
+    func get(key:String) -> String? {
+        return map[key]
+    }
+
+    @discardableResult
+    func put(key:String, val:String) -> String? {
+        let oldVal = map[key]
+        map[key] = val
+        return oldVal
+    }
+
+    @discardableResult
+    func del(key:String) -> String? {
+        return map.removeValue(forKey: key)
     }
 }
 
-let counter = Counter()
-for i in (1..<10) {
-    await counter.update(by: i)
-    let v = await counter.value
-    print(v)
-    try await Task.sleep(for: .seconds(1))
+actor BucketStore {
+    private var map: [String:Bucket] = [:]
+
+    func lookup(name: String, action: (Bucket) async -> Void) async {
+        if let bucket = map[name] {
+           await action(bucket)
+        }
+    }
+
+    func put(name:String, bucket:Bucket) {
+        map[name] = bucket
+    }
+}
+
+func newBucket(bucketStore: BucketStore, name:String) async -> Bucket {
+    let bucket = Bucket()
+    await bucketStore.put(name: name, bucket: bucket)
+    return bucket
+}
+
+// === Test ===
+
+let bucketStore = BucketStore()
+let bucketName = "langs"
+let _ = await newBucket(bucketStore: bucketStore, name: bucketName)
+
+let data = ["swift": "100", "dart": "101", "gleam": "102"]
+for (k,v) in data {
+    await bucketStore.lookup(name:bucketName) { bucket in
+        Task { await bucket.put(key:k, val:v) }
+    }
+    try await Task.sleep(for: .milliseconds(100))
+}
+
+for k in data.keys {
+    await bucketStore.lookup(name:bucketName) { bucket in
+        print(k,"->",await bucket.get(key: k) as Any)
+    }
 }
